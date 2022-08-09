b4edd4519f7fc4cad52a5af875127f6fe89d9793
Jagga Bai <dzagur@post.pl>
2022-08-09 16:44:38 +0200

    Add project files.

---
+++ Wzór2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Wzór2
+{
+    class Program
+    {
+        public static void Gwiazdki(int n)
+        {
+          int i,p, j,k,zz;
+         // nn = n;
+          int Rzad(int n) {
+                if (n % 2 == 0) { p = n / 2; }
+                else { p = (n + 1) / 2; }
+                return p;
+            }
+            zz = Rzad(n);
+            for (i = zz; i >= 1; i--)
+            {
+                for (j = zz; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k < (i * 2); k++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Gwiazdki(3);
+            Gwiazdki(15);
+        }
+    }
+}
